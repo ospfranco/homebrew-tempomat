e902c895962863e5eb0342ee7a1f02852a9d3fdc
Oscar Franco <ospfranco@protonmail.com>
2021-02-10 21:22:50 +0100

    Fix SHA

--- Formula/tempomat.rb
+++ Formula/tempomat.rb
@@ -2,7 +2,7 @@ class Tempomat < Formula
   desc "macOS continuous integration monitor"
   homepage "https://tempomat.dev"
   url "https://github.com/ospfranco/tempomat/releases/download/v3.0.0/tempomat.tar.gz"
-  sha256 "96c6f9a3a66a3e953047e9d9a43fec39683b2a1fad0927ba56540aedf3c2a5b22473866b99eef9ea983200b7aac91592b938404ffaa1fb8c72beacb2ebd3203a"
+  sha256 "96c6f9a3a66a3e953047e9d9a43fec39683b2a1fad0927ba56540aedf3c2a5b2"
   version "3.0.0"
 
   def install
